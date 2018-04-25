ec5f3bc34c379f1a0d5416c666eb00538bc5987b
jpbm <jpbmueller@gmail.com>
2018-04-25 18:03:06 -0400

    automated file splitting

--- crawler.py
+++ crawler.py
@@ -11,13 +11,16 @@ sys.path.append('../data/raw/')
 
 urls_seen = BloomFilter(max_elements=500000, error_rate=0.1)
 urls_to_do = set()
+processed = 0
 
 def initialize():
     response = requests.get(SEED_URL)
     global hyperrefs_re
     hyperrefs_re = re.compile(SEED_URL+'[a-zA-Z0-9/_-]*[.]html')
     global file
-    file = open(FILENAME,'w')
+    global filename
+    filename = filename_gen()
+    file = open(next(filename),'w')
     global file_length
     file_length = 0
     if response.status_code == 200:
@@ -58,7 +61,9 @@ def process_one(url):
         file.write(item)
         global file_length
         file_length += 1
-        print('[process_one] %i processed %i to go.' % (file_length,len(urls_to_do)),end = '\r')
+        global processed
+        processed += 1
+        print('[process_one] %i processed %i to go.' % (processed,len(urls_to_do)),end = '\r')
     elif response.status_code == 503:
         urls_to_do.update([url])
     else:
@@ -79,12 +84,27 @@ def process_many():
     """
     print('[process_many] Writing to %s' % FILENAME, end = '\r')
     while len(urls_to_do) != 0:
+        if file_length >= 1000:
+            global file
+            global filename
+            file.close()
+            file = open(next(filename),'w')
         print('[process_many] Starting loop...')
         workers = min(MAX_WORKERS,len(urls_to_do))
         with futures.ThreadPoolExecutor(workers) as executor:
             list(executor.map(process_one,list(urls_to_do)))
     file.close()
-    
+    print("Crawl complete. (you are unlikely to ever see this message...")
+
+def filename_gen():
+    i = 0
+    global FILENAME
+    path, filetype = FILENAME.split('.')
+    while True:
+        filename = path+str(i)+'.'+filetype
+        yield filename
+        i += 1
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Crawl some website.')
     parser.add_argument('seed_url',metavar='u',type=str,help='seed url for the crawler')
